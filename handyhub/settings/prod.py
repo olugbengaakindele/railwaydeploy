ba1d2a669d0c55b794649fc3d64d002cf24222a6
olugbengaakindele <akindelegbenga@gmail.com>
2026-02-09 20:18:47 -0700

    deploy v6

--- handyhub/settings/prod.py
+++ handyhub/settings/prod.py
@@ -24,11 +24,11 @@ if DB_LIVE not in ["False", False]:
     DATABASE = {
         'default' : {
             'ENGINE': 'django.db.backends.pstgresql',
-            'NAME' : os.getenv("DB_NAME"),
-            'USER' : os.getenv("DB_USER"),
-            'PASSWORD' : os.getenv("DB_PASSWORD"),
-            'HOST' : os.getenv("DB_HOST"),
-            'PORT':  os.getenv("DB_PORT"),
+            'NAME' : os.environ.get("DB_NAME"),
+            'USER' : os.environ.get("DB_USER"),
+            'PASSWORD' : os.environ.get("DB_PASSWORD"),
+            'HOST' : os.environ.get("DB_HOST"),
+            'PORT':  os.environ.get("DB_PORT"),
         }
     }
 else:
