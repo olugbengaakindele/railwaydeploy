001c9df39b572b7fc9680a88ece22df3f347924f
olugbengaakindele <akindelegbenga@gmail.com>
2026-02-10 15:10:23 -0700

    deploy v6

--- handyhub/settings/prod.py
+++ handyhub/settings/prod.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 if not SECRET_KEY:
     raise RuntimeError("SECRET_KEY is missing on Railway")
 
-ALLOWED_HOSTS =  ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",") if os.environ.get("CSRF_TRUSTED_ORIGINS") else []
 
